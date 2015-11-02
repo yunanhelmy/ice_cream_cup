3951a09ef943c09f43a74e4ba34b7983ca4f5b28
yogi bear <yogi@softwareseni.com>
2015-11-02 18:33:41 +0700

    add judge column to user table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151031141602) do
+ActiveRecord::Schema.define(version: 20151102111539) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "",    null: false
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20151031141602) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                             null: false
     t.datetime "updated_at",                             null: false
+    t.boolean  "judge",                  default: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
